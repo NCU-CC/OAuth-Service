824953722c4f6242cbeaa79c4183e58ba0e45885
jasonChiu <jason1122g@gmail.com>
2015-06-29 19:17:12 +0800

    add client restricted data objects

---
+++ oauth-data/src/main/groovy/tw/edu/ncu/cc/oauth/data/v1/management/client/ClientRestrictedIdObject.groovy
@@ -0,0 +1,9 @@
+package tw.edu.ncu.cc.oauth.data.v1.management.client
+
+
+public class ClientRestrictedIdObject {
+
+    def String client_id
+    def String reason
+
+}
---
+++ oauth-data/src/main/groovy/tw/edu/ncu/cc/oauth/data/v1/management/client/ClientRestrictedObject.groovy
@@ -0,0 +1,8 @@
+package tw.edu.ncu.cc.oauth.data.v1.management.client
+
+
+public class ClientRestrictedObject {
+
+    def String reason
+
+}
