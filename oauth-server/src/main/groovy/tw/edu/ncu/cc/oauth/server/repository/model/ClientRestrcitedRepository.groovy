f6e58e2ced2136b62329717bb9efb30d8e5e5477
jasonChiu <jason1122g@gmail.com>
2015-06-29 19:16:38 +0800

    add client restricted repository

---
+++ oauth-server/src/main/groovy/tw/edu/ncu/cc/oauth/server/repository/model/ClientRestrcitedRepository.groovy
@@ -0,0 +1,10 @@
+package tw.edu.ncu.cc.oauth.server.repository.model
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor
+import tw.edu.ncu.cc.oauth.server.model.clientRestricted.ClientRestricted
+
+public interface ClientRestrcitedRepository extends JpaRepository< ClientRestricted, Integer >, JpaSpecificationExecutor< ClientRestricted > {
+
+
+}
