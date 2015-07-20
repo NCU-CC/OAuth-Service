f08cad831372a9dea33cc1569b89c830c04290c8
jasonChiu <jason1122g@gmail.com>
2015-07-20 16:19:33 +0800

    add logs to token access api

--- oauth-server/src/main/groovy/tw/edu/ncu/cc/oauth/server/web/management/AccessTokenController.groovy
+++ oauth-server/src/main/groovy/tw/edu/ncu/cc/oauth/server/web/management/AccessTokenController.groovy
@@ -5,6 +5,7 @@ import org.springframework.core.convert.ConversionService
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import tw.edu.ncu.cc.oauth.data.v1.management.token.TokenObject
 import tw.edu.ncu.cc.oauth.server.model.accessToken.AccessToken
@@ -21,9 +22,17 @@ public class AccessTokenController {
     def ConversionService conversionService
 
     @RequestMapping( value = "token/{token}", method = RequestMethod.GET )
-    def get( @PathVariable ( "token" ) final String token ) {
+    def get( @PathVariable ( "token" ) final String token,
+             @RequestParam( value = "ip" ) String ip,
+             @RequestParam( value = "application" ) String application,
+             @RequestParam( value = "referer", required = false ) String referer ) {
 
-        def resource  = accessTokenOperations.show.process( token: token )
+        def resource  = accessTokenOperations.show.process(
+                token: token,
+                ip: ip,
+                referer: referer,
+                application: application
+        )
 
         conversionService.convert(
                 resource as AccessToken, TokenObject.class
--- oauth-server/src/test/groovy/tw/edu/ncu/cc/oauth/server/web/management/AccessTokenControllerTest.groovy
+++ oauth-server/src/test/groovy/tw/edu/ncu/cc/oauth/server/web/management/AccessTokenControllerTest.groovy
@@ -1,5 +1,6 @@
 package tw.edu.ncu.cc.oauth.server.web.management
 
+import org.springframework.transaction.annotation.Transactional
 import specification.IntegrationSpecification
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
@@ -9,6 +10,7 @@ class AccessTokenControllerTest extends IntegrationSpecification {
 
     def targetURL = "/management/v1/access_tokens"
 
+    @Transactional
     def "user can get access token info by token"() {
         given:
             def accessToken = a_accessToken()
@@ -16,12 +18,40 @@ class AccessTokenControllerTest extends IntegrationSpecification {
             def response = JSON(
                     server().perform(
                             get( targetURL + "/token/" + accessToken.token )
+                                    .param( "ip", "127.0.0.1" )
+                                    .param( "application", "test" )
                     ).andExpect(
                             status().isOk()
                     ).andReturn()
             )
         then:
             response.user == accessToken.user.name
+    }
+
+    @Transactional
+    def "user cannot get access token info by token if invalid params"() {
+        given:
+            def accessToken = a_accessToken()
+        expect:
+            server().perform(
+                    get( targetURL + "/token/" + accessToken.token )
+            ).andExpect(
+                    status().isBadRequest()
+            )
+        and:
+            server().perform(
+                    get( targetURL + "/token/" + accessToken.token )
+                            .param( "ip", "127.0.0.1" )
+            ).andExpect(
+                    status().isBadRequest()
+            )
+        and:
+            server().perform(
+                    get( targetURL + "/token/" + accessToken.token )
+                            .param( "application", "test" )
+            ).andExpect(
+                    status().isBadRequest()
+            )
     }
 
 }
