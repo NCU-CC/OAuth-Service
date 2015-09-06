7b59fcedc1500b7f35c8429577e94093152868de
jasonChiu <jason1122g@gmail.com>
2015-09-06 14:30:17 +0800

    update permission service

--- oauth-server/src/main/groovy/tw/edu/ncu/cc/oauth/server/service/permission/PermissionService.groovy
+++ oauth-server/src/main/groovy/tw/edu/ncu/cc/oauth/server/service/permission/PermissionService.groovy
@@ -3,7 +3,11 @@ package tw.edu.ncu.cc.oauth.server.service.permission
 import tw.edu.ncu.cc.oauth.server.model.permission.Permission
 
 interface PermissionService {
+
+    Permission create( Permission permission )
+    Permission delete( Permission permission )
     Permission findById( int id )
     Permission findByName( String name )
     List< Permission > findAll()
+
 }
